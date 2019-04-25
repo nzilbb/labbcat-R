9342dbaeda79676647adc2ce3bb471e27ecf002b
Robert Fromont <robert@fromont.net.nz>
2019-04-25 11:36:54 -0300

    Version 0.2-2 for LaBB-CAT version 20190425.1121

--- nzilbb.labbcat/R/nzilbb.labbcat.R
+++ nzilbb.labbcat/R/nzilbb.labbcat.R
@@ -7,7 +7,7 @@
 #' 
 #' 'LaBB-CAT' is a web-based language corpus management system and this
 #' package provides access to data stored in a 'LaBB-CAT' instance.
-#' You must have at least version 20190424.1154 of 'LaBB-CAT' to use
+#' You must have at least version 20190425.1121 of 'LaBB-CAT' to use
 #' this package.
 #' 
 #' @docType package
