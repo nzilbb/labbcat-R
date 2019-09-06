20b3a997b41444b6cc200b29859c1d2a60b9dbdb
Robert Fromont <robert@fromont.net.nz>
2019-09-06 19:18:35 -0300

    Implement getFragments to retrieve Praat TextGrids for fragments of recordings.

--- nzilbb.labbcat/R/nzilbb.labbcat.R
+++ nzilbb.labbcat/R/nzilbb.labbcat.R
@@ -166,14 +166,14 @@ http.get <- function(labbcat.url, path, parameters = NULL, content.type = "appli
 
 ## make an HTTP POST request, asking for credentials if required
 http.post <- function(labbcat.url, path, parameters, file.name) {
+    
     ## ensure labbcat base URL has a trailing slash
     if (!grepl("/$", labbcat.url)) labbcat.url <- paste(labbcat.url, "/", sep="")
 
     ## build request URL
     url <- paste(labbcat.url, path, sep="")
-    
     ## attempt the request
-    resp <- httr::POST(url,
+    resp <- httr::POST(url, 
                        httr::write_disk(file.name, overwrite=TRUE),
                        httr::timeout(.request.timeout),
                        body = parameters, encode = "form")
@@ -956,7 +956,7 @@ getMedia <- function(labbcat.url, id, trackSuffix = "", mimeType = "audio/wav") 
 #' ## Get a list of fragments
 #' wav.files <- getSoundFragments(labbcat.url, results$Transcript, results$Line, results$LineEnd)
 #' 
-#' ## Get a list of fragments with no prgress bar
+#' ## Get a list of fragments with no progress bar
 #' wav.file <- getSoundFragments(
 #'               labbcat.url, results$Transcript, results$Line, results$LineEnd, no.progress=TRUE)
 #' }
@@ -1001,6 +1001,128 @@ getSoundFragments <- function(labbcat.url, id, start, end, sampleRate = NULL, no
 
         tryCatch({
             resp <- http.post(labbcat.url, "soundfragment", parameters, file.name)
+            if (httr::status_code(resp) != 200) { # 200 = OK
+                print(paste("ERROR: ", httr::http_status(resp)$message))
+                if (httr::status_code(resp) != 404) { # 404 means the audio wasn't on the server
+                    ## some other error occurred so print what we got from the server
+                    print(readLines(file.name))
+                }
+                file.remove(file.name)
+                file.name <<- NULL
+            } else {
+                content.disposition <- as.character(httr::headers(resp)["content-disposition"])
+                content.disposition.parts <- strsplit(content.disposition, "=")
+                if (length(content.disposition.parts[[1]]) > 1
+                    && file.name != content.disposition.parts[[1]][2]) {
+                    ## file name is specified, so use it
+                    final.file.name <- paste(dir, content.disposition.parts[[1]][2], sep="")
+                    file.rename(file.name, final.file.name)
+                    file.name <- final.file.name
+                }
+            }
+        }, error = function(e) {
+            print(paste("ERROR:", e))
+            file.name <<- NULL
+        })
+        file.names <- append(file.names, file.name)
+        
+        if (!is.null(pb)) setTxtProgressBar(pb, r)
+        r <- r+1
+    } ## next row
+    if (!is.null(pb)) close(pb)
+    return(file.names)   
+}
+
+#' Gets fragments of annotation graphs (transcripts) from 'LaBB-CAT', 
+#' converted to a given format (by default, Praat TextGrid)
+#'
+#' @param labbcat.url URL to the LaBB-CAT instance
+#' @param id The graph ID (transcript name) of the sound recording, or
+#'     a vector of graph IDs. 
+#' @param start The start time in seconds, or a vector of start times.
+#' @param end The end time in seconds, or a vector of end times.
+#' @param layerId A vector of layer IDs.
+#' @param contentType Optional content-type - currently only "text/praat-textgrid"
+#'     is supported.
+#' @param no.progress Optionally suppress the progress bar when
+#'     multiple fragments are  specified - TRUE for no progress bar.
+#' @return The name of the file, which is saved in the current
+#'     directory, or a list of names of files, if multiple
+#'     id's/start's/end's were specified 
+#'
+#' If a list of files is returned, they are in the order that they
+#'     were returned by the server, which *should* be the order that
+#'     they were specified in the id/start/end lists.
+#' 
+#' @examples
+#' \dontrun{
+#' ## define the LaBB-CAT URL
+#' labbcat.url <- "https://labbcat.canterbury.ac.nz/demo/"
+#' 
+#' ## Get the 5 seconds starting from 10s after the beginning of a recording
+#' textgrid.file <- getFragments(labbcat.url, "AP2505_Nelson.eaf", 10.0, 15.0,
+#'     c("transcript", "phonemes")) 
+#' 
+#' ## Load some search results previously exported from LaBB-CAT
+#' results <- read.csv("results.csv", header=T)
+#' 
+#' ## Get a list of fragment TextGrids, including the utterances, transcript, andd phonemes layers
+#' textgrid.files <- getFragments(
+#'     labbcat.url, results$Transcript, results$Line, results$LineEnd,
+#'     c("utterances", "transcript", "phonemes"))
+#' 
+#' ## Get a list of fragment TextGrids with no progress bar
+#' textgrid.files <- getFragments(
+#'     labbcat.url, results$Transcript, results$Line, results$LineEnd, no.progress=TRUE)
+#' }
+#' @keywords sample sound fragment wav
+#' 
+getFragments <- function(labbcat.url, id, start, end, layerId, mimeType = "text/praat-textgrid", no.progress=FALSE) {
+
+    if (length(id) == 1) { ## one fragment
+        dir <- ""
+    } else { ## multiple fragments
+        ## save fragments into their own directory
+        dir <- "fragments"
+        if (file.exists(dir)) {
+            ## ensure it's a new directory by adding a number
+            n <- 1
+            new.dir = paste(dir,"(",n,")", sep="")
+            while (file.exists(new.dir)) {
+                n <- n + 1
+                new.dir = paste(dir,"(",n,")", sep="")
+            } # next try
+            dir <- new.dir
+        }
+        dir.create(dir)
+        ## add trailing slash
+        dir <- paste(dir, .Platform$file.sep, sep="")
+    }
+    
+    pb <- NULL
+    if (!no.progress && length(id) > 1) {
+        pb <- txtProgressBar(min = 0, max = length(id), style = 3)        
+    }
+
+    ## create a list of repeated layerId parameters
+    layerParameters <- list()
+    mapply(function(l) { layerParameters <<- c(layerParameters, list(layerId=l)) }, layerId)
+
+
+    ## loop throug each triple, getting fragments individually
+    ## (we could actually pass the lot to LaBB-CAT in one go and get a ZIP file back
+    ##  but then we can't be sure the results contain a row for every fragment specified
+    ##  and we can't display a progress bar)
+    file.names = c()
+    r <- 1
+    for (graph.id in id) {
+        parameters <- list(mimeType=mimeType, id=graph.id, start=start[r], end=end[r])
+        ## add layerId parameters
+        parameters <- c(parameters, layerParameters)
+        file.name <- paste(dir, stringr::str_replace(graph.id, "\\.[^.]+$",""), "__", start[r], "-", end[r], ".TextGrid", sep="")
+        
+        tryCatch({
+            resp <- http.post(labbcat.url, "convertfragment", parameters, file.name)
             if (httr::status_code(resp) != 200) { # 200 = OK
                 print(paste("ERROR: ", httr::http_status(resp)$message))
                 if (httr::status_code(resp) != 404) { # 404 means the audio wasn't on the server
