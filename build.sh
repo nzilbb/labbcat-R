9847401a73eb9f72bdc717045febe82023527933
Robert Fromont <robert@fromont.net.nz>
2020-08-26 12:12:11 -0300

    Move package files into repository root so automatically generated document links to source files work, and to simplify build process.

--- build.sh
+++ build.sh
@@ -5,15 +5,13 @@ echo remove older versions
 rm nzilbb.labbcat_*.tar.gz
 
 echo generate documentation ...
-R -e "devtools::document('nzilbb.labbcat')"
+R -e "devtools::document()"
+R -e "pkgdown::build_site()"
 
 echo build nzilbb.labbcat ...
-R CMD build nzilbb.labbcat
+R CMD build
 
 echo check nzilbb.labbcat ...
 R CMD check --as-cran nzilbb.labbcat_*.tar.gz
-
-echo build documentation site ...
-./build-docs.sh
 
 echo finished.
