c68a90ab98b568b84f347b2d13170c925474d8c6
Robert Fromont <robert@fromont.net.nz>
2021-04-06 17:35:30 -0300

    getFragments in a single request, so that Kaldi fragments are supported.

--- R/getFragments.R
+++ R/getFragments.R
@@ -1,5 +1,11 @@
-#' Gets fragments of annotation transcript from 'LaBB-CAT', 
-#' converted to a given format (by default, Praat TextGrid)
+#' Gets fragments transcript in a given format.
+#' 
+#' This function gets fragments of transcripts from 'LaBB-CAT', 
+#' converted to a given format (by default, Praat TextGrid).
+#'
+#' \emph{NB} Although many formats will generate exactly one file for each interval
+#'      (e.g. mimeType=text/praat-textgrid), this is not guaranted; some formats generate
+#'      a single file or a fixed collection of files regardless of how many fragments there are.
 #'
 #' @param labbcat.url URL to the LaBB-CAT instance
 #' @param id The transcript ID (transcript name) of the sound recording, or
@@ -69,57 +75,58 @@ getFragments <- function(labbcat.url, id, start, end, layerIds, mimeType = "text
         }
     }
     
-    pb <- NULL
-    if (interactive() && length(id) > 1) {
-        pb <- txtProgressBar(min = 0, max = length(id), style = 3)        
-    }
-
-    ## create a list of repeated layerId parameters
+    ## create list of repeated parameters
     layerParameters <- list()
     mapply(function(l) { layerParameters <<- c(layerParameters, list(layerId=l)) }, layerIds)
+    idParameters <- list()
+    mapply(function(l) { idParameters <<- c(idParameters, list(id=l)) }, id)
+    startParameters <- list()
+    mapply(function(l) { startParameters <<- c(startParameters, list(start=l)) }, start)
+    endParameters <- list()
+    mapply(function(l) { endParameters <<- c(endParameters, list(end=l)) }, end)
 
-    ## loop throug each triple, getting fragments individually
-    ## (we could actually pass the lot to LaBB-CAT in one go and get a ZIP file back
-    ##  but then we can't be sure the results contain a row for every fragment specified
-    ##  and we can't display a progress bar)
+    parameters <- list(mimeType=mimeType)
+    ## add list parameters
+    parameters <- c(parameters, layerParameters)
+    parameters <- c(parameters, idParameters)
+    parameters <- c(parameters, startParameters)
+    parameters <- c(parameters, endParameters)
+    file.name <- paste(dir, "fragments.zip", sep="")
+
     file.names = c()
-    r <- 1
-    for (graph.id in id) {
-        parameters <- list(mimeType=mimeType, id=graph.id, start=start[r], end=end[r])
-        ## add layerId parameters
-        parameters <- c(parameters, layerParameters)
-        file.name <- paste(dir, stringr::str_replace(graph.id, "\\.[^.]+$",""), "__", start[r], "-", end[r], ".TextGrid", sep="")
-        
-        tryCatch({
-            resp <- http.post(labbcat.url, "api/serialize/fragment", parameters, file.name)
-            if (httr::status_code(resp) != 200) { # 200 = OK
-                print(paste("ERROR: ", httr::http_status(resp)$message))
-                if (httr::status_code(resp) != 404) { # 404 means the audio wasn't on the server
-                    ## some other error occurred so print what we got from the server
-                    print(readLines(file.name))
-                }
-                file.remove(file.name)
-                file.name <<- NULL
-            } else {
-                content.disposition <- as.character(httr::headers(resp)["content-disposition"])
-                content.disposition.parts <- strsplit(content.disposition, "=")
-                if (length(content.disposition.parts[[1]]) > 1
-                    && file.name != content.disposition.parts[[1]][2]) {
-                    ## file name is specified, so use it
-                    final.file.name <- paste(dir, content.disposition.parts[[1]][2], sep="")
-                    file.rename(file.name, final.file.name)
-                    file.name <- final.file.name
-                }
+    tryCatch({
+        resp <- http.post(labbcat.url, "api/serialize/fragment", parameters, file.name)
+        if (httr::status_code(resp) != 200) { # 200 = OK
+            print(paste("ERROR: ", httr::http_status(resp)$message))
+            if (httr::status_code(resp) != 404) { # 404 means the audio wasn't on the server
+                ## some other error occurred so print what we got from the server
+                print(readLines(file.name))
             }
-        }, error = function(e) {
-            print(paste("ERROR:", e))
+            file.remove(file.name)
             file.name <<- NULL
-        })
-        file.names <- append(file.names, file.name)
-        
-        if (!is.null(pb)) setTxtProgressBar(pb, r)
-        r <- r+1
-    } ## next row
-    if (!is.null(pb)) close(pb)
+        } else {
+            content.disposition <- as.character(httr::headers(resp)["content-disposition"])
+            content.disposition.parts <- strsplit(content.disposition, "=")
+            if (length(content.disposition.parts[[1]]) > 1
+                && file.name != content.disposition.parts[[1]][2]) {
+                ## file name is specified, so use it
+                final.file.name <- paste(dir, content.disposition.parts[[1]][2], sep="")
+                file.rename(file.name, final.file.name)
+                file.name <- final.file.name
+            }
+
+            ## list the files
+            file.names <- paste(dir, unzip(file.name, list=T)$Name, sep="")
+
+            ## unzip result
+            unzip(file.name, exdir=dir)
+
+            ## remove zip file
+            file.remove(file.name)
+        }
+    }, error = function(e) {
+        print(paste("ERROR:", e))
+        file.name <<- NULL
+    })
     return(file.names)   
 }
