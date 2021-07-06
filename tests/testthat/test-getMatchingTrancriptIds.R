5dcd303d9dfd13e493e4f0e0286c3c132c79bebe
Robert Fromont <robert@fromont.net.nz>
2021-07-06 19:56:39 -0300

    Update unit test to use new expression syntax.

--- tests/testthat/test-getMatchingTrancriptIds.R
+++ tests/testthat/test-getMatchingTrancriptIds.R
@@ -4,8 +4,8 @@ test_that("getMatchingTranscriptIds works", {
     skip_on_cran() # don't run tests that depend on external resource on CRAN
     if (!is.null(labbcatCredentials(labbcat.url, "demo", "demo"))) skip("Server not available")
 
-    ids <- getMatchingTranscriptIds(labbcat.url, "id MATCHES 'BR.+'")
-    expect_equal(length(ids), 6)
+    ids <- getMatchingTranscriptIds(labbcat.url, "/^BR.+/.test(id)")
+    expect_equal(length(ids), 5)
     expect_true("BR946_RodgerCurragh.eaf" %in% ids)
     expect_false("QB247_Jacqui.eaf" %in% ids)
 })
