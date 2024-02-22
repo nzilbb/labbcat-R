0be45fd2445290dbf24c542f04e43d5b7cb45891
Robert Fromont <robert@fromont.net.nz>
2024-02-22 14:43:55 -0300

    Add expressionFromAttributeValuesCount

---
+++ R/expressionFromAttributeValuesCount.R
@@ -0,0 +1,44 @@
+#' Generates a query expression for matching a transcript/participant attribute, for use with
+#' \link{getMatches}.
+#'
+#' This function generates a query expression fragment which can be passed as
+#' the transcript.expression or participant.expression parameter of \link{getMatches}, (or
+#' the expression parameter of \link{getMatchingTranscriptIds} or
+#' \link{getMatchingParticipantIds}) matching by the number of values for a given
+#' attribute. 
+#'
+#' The attribute defined by transcript.attribute is expected to have possibly more than
+#' one value, although single-value attributes may have 0 or 1 values, and this function
+#' can be used to distinguish these two possibilities as well.
+#'
+#' @param transcript.attribute The transcript attribute to filter by. 
+#' @param comparison A string representing the operator to use for comparison, one of
+#' "<", "<=", "==", "!=", ">=", ">". 
+#' @param count The number to compare the count of values to.
+#' @return A transcript query expression which can be passed as the
+#' transcript.expression parameter of \link{getMatches} or the expression parameter
+#' of \link{getMatchingTranscriptIds}
+#' 
+#' @seealso \link{expressionFromAttributeValues}
+#' @seealso \link{expressionFromTranscriptTypes}
+#' @seealso \link{expressionFromIds}
+#' @seealso \link{getMatches}
+#' @examples
+#' \dontrun{
+#' ## Search only transcripts including multilingual participants
+#' results <- getMatches(labbcat.url, list(segment="I"),
+#'                       participant.expression = expressionFromAttributeValuesCount(
+#'                             "participant_languages", ">=", 2))
+#'
+#' ## Search only transcripts with no restrictions specified
+#' results <- getMatches(labbcat.url, list(segment="I"),
+#'                       transcript.expression = expressionFromAttributeValuesCount(
+#'                             "transcript_restrictions", "==", 0))
+#' }
+#' @keywords search
+#' 
+expressionFromAttributeValuesCount <- function(transcript.attribute, comparison = "==", count) {
+    escapedAttribute <- gsub("'","\\\\'", transcript.attribute)
+    return(paste("labels('",escapedAttribute,"').length", " ", comparison, " ", count,
+                     sep=""))
+}
